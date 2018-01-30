bf0086a66995016f83a7fea2d77f4fc8bd17825b
djdeo <32230775+djdeo@users.noreply.github.com>
2018-01-30 14:27:24 +0800

    Vue showAlert()

---
+++ showAlert/main.js
@@ -0,0 +1,22 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+    	active: false,
+        alertMsg: ''
+    },
+    methods:{
+    	alertInfo:function(cls, msg){
+    		this.active = true
+    		this.alertMsg = cls
+    		document.getElementById('msg').innerHTML = msg
+    		setTimeout(()=>this.active = false,2000)
+    	},
+    	showAlertS: function(){
+    		this.alertInfo('alert-success', 'Success!!!!')
+    	},
+    	showAlertD: function(){
+    		this.alertInfo('alert-danger', 'danger!!!!')
+
+    	}
+    }
+})
